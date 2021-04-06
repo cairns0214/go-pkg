74ad7ee59d7d874614fde768e326db9bab0a1a5d
LvJinyu <lvjinyu@myhexin.com>
2021-04-06 21:55:37 +0800

    ip通用包

---
+++ ip/ip.go
@@ -0,0 +1,26 @@
+package ip
+
+import (
+	"net"
+)
+
+// 获取本地ip列表
+func GetIPS() (ipList []string) {
+	netInterfaces, err := net.Interfaces()
+	if err != nil {
+		return
+	}
+	for i := 0; i < len(netInterfaces); i++ {
+		if (netInterfaces[i].Flags & net.FlagUp) != 0 {
+			addrs, _ := netInterfaces[i].Addrs()
+			for _, address := range addrs {
+				if ipnet, ok := address.(*net.IPNet); ok && !ipnet.IP.IsLoopback() {
+					if ipnet.IP.To4() != nil {
+						ipList = append(ipList, ipnet.IP.String())
+					}
+				}
+			}
+		}
+	}
+	return
+}
